47a56c00dcefcab6c36c1ffa9902a6d117e301ee
Simon Castro Valencia <simonestebanc23@gmail.com>
2024-05-08 16:07:05 -0500

    "Encapsulamiento -implementacion de metodos para manipulacion de propiedades por : santiago castro valencia"

--- Persona.java
+++ Persona.java
@@ -1,5 +1,32 @@
 public class Persona{
     private String Nombre;
     private int Edad;
-    private String FechaDeNacimiento;    
+    private String FechaDeNacimiento;   
+    
+        public void establecerNombre(String Nombre){
+            this.Nombre = Nombre;
+        }
+    
+        public String obtenerNombre(){
+            return this.Nombre;
+        }
+    
+        public void establecerEdad(int Edad){ 
+        
+                this.Edad = Edad;
+        }  
+        
+    
+        public int obtenerEdad(){
+            return this.Edad;
+        }
+    
+        public void establecerFechaDeNacimiento(String FechaDeNacimiento){
+            this.FechaDeNacimiento = FechaDeNacimiento;
+        }
+    
+        public String obtenerFechaDeNacimiento(){
+            return this.FechaDeNacimiento;
+        }
+    
 }

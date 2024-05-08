dd46077e111a09121b30a521203780392ee43e80
Simon Castro Valencia <simonestebanc23@gmail.com>
2024-05-08 17:47:59 -0500

    "Herencia-sobrescritura del metodo toString() por: santiago castro valencia"

--- Estudiante.java
+++ Estudiante.java
@@ -1,4 +1,12 @@
 
+import java.util.Date;
 public class Estudiante  extends Persona{
     private String grado;
-}
+
+    
+        public Estudiante(String Nombre, int Edad, Date FechaDeNacimiento, String grado){
+            super();
+
+        }
+    
+    }
--- Persona.java
+++ Persona.java
@@ -1,54 +1,56 @@
-import java.sql.Date;
+import java.util.Date;
 import java.time.LocalDate;
 
-public class Persona{
+public class Persona {
     private String Nombre;
     private int Edad;
-    private Date FechaDeNacimiento;   
-    
-        public void establecerNombre(String Nombre){
-            if(Nombre == "" || Nombre == null ){
-                System.out.println("El nombre es invalido");
-            }else{
-                this.Nombre=Nombre;
-            }
-        }
-    
-        public String obtenerNombre(){
-            return this.Nombre;
-        }
-    
-        public void establecerEdad(int Edad){ 
-            if (Edad < 0) {
-                System.out.println("La edad debe ser mayor a 0");
-            }else{
-                this.Edad = Edad;
-            }
-        }  
-        
-    
-        public int obtenerEdad(){
-            return this.Edad;
-        }
-    
-        public void establecerFechaDeNacimiento(Date FechaDeNacimiento){
-            if(FechaDeNacimiento == null ){
-                System.out.println("La fecha es invalida");
-            }else{
-                this.FechaDeNacimiento=FechaDeNacimiento;
-            }
-        }
-        
-    
-        public Date obtenerFechaDeNacimiento(){
-            return this.FechaDeNacimiento;
-        }
+    private Date FechaDeNacimiento;
 
-        public LocalDate CalcularFechaDeNacimiento(){
-            if (this.Edad < 0){
-                System.out.println("Edad no valida");
-            }
-            return LocalDate.now().minusYears(this.Edad);
+    public void establecerNombre(String Nombre) {
+        if (Nombre == "" || Nombre == null) {
+            System.out.println("El nombre es invalido");
+        } else {
+            this.Nombre = Nombre;
         }
-    
+    }
+
+    public String obtenerNombre() {
+        return this.Nombre;
+    }
+
+    public void establecerEdad(int Edad) {
+        if (Edad < 0) {
+            System.out.println("La edad debe ser mayor a 0");
+        } else {
+            this.Edad = Edad;
+        }
+    }
+
+    public int obtenerEdad() {
+        return this.Edad;
+    }
+
+    public void establecerFechaDeNacimiento(Date FechaDeNacimiento) {
+        if (FechaDeNacimiento == null) {
+            System.out.println("La fecha es invalida");
+        } else {
+            this.FechaDeNacimiento = FechaDeNacimiento;
+        }
+    }
+
+    public Date obtenerFechaDeNacimiento() {
+        return this.FechaDeNacimiento;
+    }
+
+    public LocalDate CalcularFechaDeNacimiento() {
+        if (this.Edad < 0) {
+            System.out.println("Edad no valida");
+        }
+        return LocalDate.now().minusYears(this.Edad);
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + Nombre + ", Edad: " + Edad + ", Fecha de nacimiento: " + FechaDeNacimiento;
+    }
 }

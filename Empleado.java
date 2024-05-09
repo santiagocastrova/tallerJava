3f4d163f58a5cd5509b8893a11254b069f9ee5dd
Simon Castro Valencia <simonestebanc23@gmail.com>
2024-05-08 22:12:39 -0500

    "Herencia y Polimorfismo - implementacion del metodo calcularsalario() en clase gerente por: santiago castro valencia"

--- Empleado.java
+++ Empleado.java
@@ -3,6 +3,25 @@
     private String Nombre;
     private float Salario;
 
+    public Empleado(String Nombre, float Salario){
+        Nombre = Nombre;
+        Salario = Salario;
+    }
+
+    public String getNombre(){
+        return Nombre;
+    }
+    public void setNombre(String Nombre){
+        Nombre = Nombre;
+    }
+
+    public float getSalario(){
+        return Salario;
+    }
+    public void setSalario(float Salario){
+        Salario = Salario;
+    }
+
     public void RealizarTarea(){
         System.out.println("realizando tarea laboral");
     }
--- Gerente.java
+++ Gerente.java
@@ -1,3 +1,25 @@
 public class Gerente extends Empleado{
     
+    private float Bono;
+    private float Horas;
+    private float v_horas;
+
+    public Gerente(String Nombre, float Salario, float Bono, float Horas, float v_horas){
+        super(Nombre, Salario);
+        this.Bono = Bono;
+        this.Horas = Horas;
+        this.v_horas = v_horas;
+
+    }
+
+    public float getBono(){
+        return Bono;
+    }
+    public void setBono(float Bono){
+        this.Bono = Bono;
+    }
+
+    public float CalcularSalario(){
+        return (Horas*v_horas)+Bono;
+    }
 }

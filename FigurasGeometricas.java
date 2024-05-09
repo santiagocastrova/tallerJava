9946ba6ed3c143f553a85b9ad67b5e24785c8438
Simon Castro Valencia <simonestebanc23@gmail.com>
2024-05-08 19:19:28 -0500

    "Encapsulamiento y Herencia - implementacion de metodos para manipulacion de propiedades por: santiago castro valencia"

--- FigurasGeometricas.java
+++ FigurasGeometricas.java
@@ -2,4 +2,21 @@ public class FigurasGeometricas {
     private String Nombre;
     private String Color;
 
+    public void establecerNombre(String Nombre){
+        this.Nombre = Nombre;
+    }
+
+    public String obtenerNombre(String Nombre){
+       return this.Nombre;
+    }
+
+    public void establecerColor(String Color){
+        this.Color = Color;
+    }
+
+    public String obtenerColor(String Color){
+       return this.Color;
+    }
+
+
 }

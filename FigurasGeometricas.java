d7db96e578bdbc5ac0d49511baca05515f68b443
Simon Castro Valencia <simonestebanc23@gmail.com>
2024-05-08 19:25:59 -0500

    "Encapsulamiento y Herencia- implementacion de subclases por: santiago castro valencia"

--- FigurasGeometricas.java
+++ FigurasGeometricas.java
@@ -2,21 +2,39 @@ public class FigurasGeometricas {
     private String Nombre;
     private String Color;
 
-    public void establecerNombre(String Nombre){
+    public void establecerNombre(String Nombre) {
         this.Nombre = Nombre;
     }
 
-    public String obtenerNombre(String Nombre){
-       return this.Nombre;
+    public String obtenerNombre(String Nombre) {
+        return this.Nombre;
     }
 
-    public void establecerColor(String Color){
+    public void establecerColor(String Color) {
         this.Color = Color;
     }
 
-    public String obtenerColor(String Color){
-       return this.Color;
+    public String obtenerColor(String Color) {
+        return this.Color;
     }
 
+    public class Rectangulo extends FigurasGeometricas {
+        private double longitud;
+        private double ancho;
 
+        public Rectangulo(double longitud, double ancho) {
+            this.longitud = longitud;
+            this.ancho = ancho;
+        }
+
+    }
+
+    public class Circulo extends FigurasGeometricas {
+        private double radio;
+
+        public Circulo(double radio) {
+            this.radio = radio;
+        }
+
+    }
 }
